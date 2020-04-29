851a15d3f05c4809be43b4b9842bb972ab26459a
Pearl Zeng <zhuzeng@umich.edu>
2020-04-28 22:20:56 -0400

    Link `AddTodoForm` with `TodoList` and display the todos added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import * as Types from 'typings';
 
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Button from '@material-ui/core/Button';
+import AddTodoForm from 'components/AddTodoForm';
+import TodoItem from 'components/TodoItem';
 
 interface Props {
   value: Types.TodoList;
@@ -11,18 +15,44 @@ interface Props {
 
 function TodoList({ value }: Props) {
   const classes = useStyles();
-  const { name } = value;
+  const { id: listId, name } = value;
+  const [addingTodo, setAddingTodo] = useState(false);
+  const todoItemsState = useSelector(
+    (state: Types.RootState) => state.todoItems,
+  );
+
+  const todoItems = Object.values(todoItemsState);
+
+  const handleAddTodo = () => {
+    setAddingTodo(true);
+  };
+
+  const handleDoneAddTodo = () => {
+    setAddingTodo(false);
+  };
 
   return (
-    <List
-      className={classes.root}
-      aria-labelledby={name}
-      subheader={
-        <ListSubheader component="div" id={name}>
-          {name}
-        </ListSubheader>
-      }
-    ></List>
+    <>
+      <List
+        className={classes.root}
+        aria-labelledby={name}
+        subheader={
+          <ListSubheader component="div" id={name}>
+            {name}
+          </ListSubheader>
+        }
+      >
+        <Button onClick={handleAddTodo}>Add Todo</Button>
+        {todoItems.map((item) => (
+          <TodoItem value={item} key={item.id} />
+        ))}
+      </List>
+      <AddTodoForm
+        open={addingTodo}
+        listId={listId}
+        onClose={handleDoneAddTodo}
+      />
+    </>
   );
 }
 
